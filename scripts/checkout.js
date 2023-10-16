701e5da1abb8287142c41a4f159d728e9c65897f
ddnanam <dd.nanam@gmail.com>
2023-10-16 21:05:48 +0000

    delet products from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -10,7 +10,6 @@ cart.forEach((cartItem) => {
   products.forEach((product) => {
     if (product.id === productId) {
       matchingProduct = product;
-      console.log(product);
     }
   });
 
@@ -35,7 +34,9 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                    matchingProduct.id
+                  }" >
                     Delete
                   </span>
                 </div>
@@ -85,5 +86,13 @@ cart.forEach((cartItem) => {
           </div>
     `;
 });
-console.log(cartSummary);
+
 document.querySelector(".js-order-summary").innerHTML = cartSummary;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;    
+    removeFromCart(productId);
+    
+  });
+});
